83123dbb8c960f34aba7247308b211c24a4b285c
mahaafaisal <mahafaisalmubarak@gmail.com>
2024-04-15 11:27:39 +0400

    cd fixed

--- execution/ft_cd.c
+++ execution/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: mafaisal <mafaisal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 15:37:04 by diahmed           #+#    #+#             */
-/*   Updated: 2024/04/08 18:37:30 by mafaisal         ###   ########.fr       */
+/*   Updated: 2024/04/15 11:25:04 by mafaisal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,17 @@ void	ft_cd(t_mshell *shell)
 		else
 		{
 			ft_putendl_fd("cd: HOME not set", 2);
-			// ft_free(shell->command);
 			return ;
 		}
 	}
 	else
 		directory = shell->command[1];
 	getcwd(pwd, sizeof(pwd));
-	add_var(&shell->params, ft_str_join("OLDPWD=", pwd));
 	if (chdir(directory) < 0)
+	{
 		perror("cd");
-	getcwd(pwd, sizeof(directory));
-	add_var(&shell->params, ft_str_join("PWD=", pwd));
-	// ft_free(shell->command);
+		return ;
+	}
+	add_var(&shell->params, ft_str_join("OLDPWD=", pwd));
+	add_var(&shell->params, ft_str_join("PWD=", getcwd(pwd, sizeof(pwd))));
 }
