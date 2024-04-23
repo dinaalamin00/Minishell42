e052c988cf399350067b4fd3a609b0a4e798107a
mahaafaisal <mahafaisalmubarak@gmail.com>
2024-04-23 12:55:34 +0400

    modified execute external

--- execution/execute_external.c
+++ execution/execute_external.c
@@ -3,31 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   execute_external.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: diahmed <diahmed@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mafaisal <mafaisal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 11:02:27 by diahmed           #+#    #+#             */
-/*   Updated: 2024/04/22 17:42:04 by diahmed          ###   ########.fr       */
+/*   Updated: 2024/04/23 12:55:00 by mafaisal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-char	*is_accessible(char *path, char *command)
+void	is_accessible(t_mshell *shell, char **path, char **env)
 {
 	char	*temp;
 	char	*exec;
+	int		i;
 
-	if (!access(command, X_OK))
-		return (command);
-	exec = ft_str_join(path, "/");
-	if (exec)
-		exec = ft_strjoin(exec, command);
+	i = -1;
+	while (path && path[++i])
+	{
+		exec = ft_str_join(path[i], "/");
+		if (exec)
+			exec = ft_strjoin(exec, shell->command[0]);
+		else
+			(free(exec), malloc_error(shell, 1, 1));
+		if (exec)
+		{
+			if (!access(exec, F_OK | X_OK))
+			{
+				if (execve(exec, shell->command, env) < 0)
+					(perror("execve"), free_shell(shell, 1, 1));
+			}
+		}
+		else
+			malloc_error(shell, 1, 1);
+	}
+}
+
+void	access_absolute(t_mshell *shell, char **env)
+{
+	if (!access(shell->command[0], F_OK | X_OK))
+		execve(shell->command[0], shell->command, env);
+}
+
+char	**get_path(t_mshell *shell, char **env)
+{
+	t_param	*path;
+
+	path = get_param(shell->params, "PATH");
+	if (path && path->value)
+		return (ft_split((path->value), ':'));
 	else
-		return (NULL);
-	if (!access(exec, F_OK | X_OK))
-		return (exec);
-	if (exec)
-		free(exec);
+	{
+		command_error(shell, NULL, ": No such file or directory", 127);
+		free_shell(shell, 1, shell->exit_code);
+	}
 	return (NULL);
 }
 
@@ -41,24 +70,15 @@ int	execute_external(t_mshell *shell, char **env)
 	pid = fork();
 	if (pid == 0)
 	{
-		if (!access(shell->command[0], F_OK | X_OK))
-			execve(shell->command[0], shell->command, env);
-		path = ft_split((get_param(shell->params, "PATH")->value), ':');
-		i = 0;
-		while (path && path[i])
-		{
-			exec = is_accessible (path[i++], shell->command[0]);
-			if (exec)
-			{
-				if (execve(exec, shell->command, env) < 0)
-					perror("Execve");
-				// (perror("execve"), free_shell(shell, 1, 1));
-			}
-		}
+		access_absolute(shell, env);
+		path = get_path(shell, env);
 		if (!path || !path[0])
+		{
 			command_error(shell, NULL, ": No such file or directory", 127);
-		else
-			command_error(shell, NULL, ": command not found", 127);
+			free_shell(shell, 1, 127);
+		}
+		is_accessible(shell, path, env);
+		command_error(shell, NULL, ": command not found", 127);
 		(ft_free(path), free_shell(shell, 1, 127));
 	}
 	else
@@ -66,6 +86,5 @@ int	execute_external(t_mshell *shell, char **env)
 		waitpid(pid, &shell->exit_code, 0);
 		shell->exit_code = WEXITSTATUS(shell->exit_code);
 	}
-	return (1);
+	return (0);
 }
-//error msg for null path "no such file or directory"
