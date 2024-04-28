fad483bb1692827d9c841d7a909c8877db4e03bb
dinaalamin00 <115608805+dinaalamin00@users.noreply.github.com>
2024-04-28 14:45:06 +0000

    pipe validity

--- lex/pipe_validity.c
+++ lex/pipe_validity.c
@@ -2,19 +2,30 @@
 
 static int	valid_pipe(char *user_input)
 {
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = user_input;
+	while (str_pipe(str))
+	{
+		i = 0;
+		str = str_pipe(str) + 1;
+		while (str[i] == 32 && str[i])
+			i++;
+		if (!str[i] || str[i] == '|')
+			return (0);
+	}
 	return (1);
 }
 
 bool	pipe_validity(t_mshell *shell)
 {
-	// if (ft_strset(shell->user_input, "\"\'"))
-	// {
-	// 	if (!valid_pipe(shell->user_input))
-	// 	{
-	// 		ft_putendl_fd("syntax error, unexpected token", 2);
-	// 		free_shell(shell, 0, -1);
-	// 		return (FAILURE);
-	// 	}
-	// }
+	if (!valid_pipe(shell->user_input))
+	{
+		ft_putendl_fd("syntax error, unexpected token", 2);
+		free_shell(shell, 0, -1);
+		return (FAILURE);
+	}
 	return (SUCCESS);
 }
